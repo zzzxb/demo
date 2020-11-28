67c7cef05df0dc13089cdc8ef1d8fab9d88acca5
Zzzxb <idearfree@outlook.com>
2020-11-29 01:51:41 +0800

    test api

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello Demo!\n";
+    }
+}
